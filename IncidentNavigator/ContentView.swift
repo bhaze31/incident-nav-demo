be63b78d4d86c1700167fc7b058f561496f769f1
Brian Hasenstab <brian.hasenstab31@gmail.com>
2021-03-10 13:46:21 -0500

    Starter Version

--- IncidentNavigator/ContentView.swift
+++ IncidentNavigator/ContentView.swift
@@ -8,33 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
-    @ObservedObject var incident: Incident
-
     var body: some View {
-        VStack {
-            Form {
-                Section(header: Text("Incident Status")) {
-                    Picker("Incident State", selection: $incident.status) {
-                        ForEach(IncidentState.allCases, id: \.self) { state in
-                            Text(state.rawValue)
-                        }
-                    }
-                }
-                Section(header: Text("Description")) {
-                    TextEditor(text: $incident.desc)
-                }
-            }
-
-            Spacer()
-        }
-        .navigationTitle(incident.name)
+        Text("Hello World")
+            .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        NavigationView {
-            ContentView(incident: demoIncidents[1])
-        }
+        ContentView()
     }
 }
